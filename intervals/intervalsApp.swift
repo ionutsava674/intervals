e1a32e62dedf6198b7bdc0fb844f694c5301fed4
Ionut Sava <object.inspector@gmail.com>
2022-12-12 14:39:14 +0200

    Initial Commit

---
+++ intervals/intervalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  intervalsApp.swift
+//  intervals
+//
+//  Created by Ionut on 12.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct intervalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
